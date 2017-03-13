c6b46fd9a67a4ba48c0f5601b28d8c365f3914e8
William Benton <willb@redhat.com>
2017-03-13 12:57:16 -0500

    added hello-world test

---
+++ src/main/scala/com/redhat/et/testcases/Main.scala
@@ -0,0 +1,37 @@
+package com.redhat.et.testcases
+
+import scopt.OptionParser
+
+case class TestConfig(cases: Seq[String])
+
+trait Testcase {
+  def run: Unit
+}
+
+object Main {
+  val TESTCASES = Map[String, Testcase]("hello" -> new Hello())
+
+  def main(args: Array[String]) {
+    val parser = new OptionParser[TestConfig]("testcases") {
+      head("testcases", "0.0.1")
+
+      help("help").text("prints this usage text")
+
+      arg[String]("<test>...").unbounded().optional().action( (x, c) =>
+	c.copy(cases = c.cases :+ x) ).text("test cases to run")
+
+      note("valid test cases include: \n  * hello (\"hello, world\")\n  * spark (run a trivial Spark job)\n  * geometry (property-based testing of computational geometry code)\n\nif no test case is specified, run all")
+    }
+    
+    parser.parse(args, TestConfig(Seq())) match {
+      case Some(config) =>
+	val cases = config.cases match {
+	  case Seq() => TESTCASES.keys
+	  case x => x
+	}
+
+	cases.foreach(c => TESTCASES.get(c).map(v => v.run).getOrElse(Console.println(s"unknown test case $c (use --help for info)")))
+      case None => ()
+    }
+  }
+}
---
+++ src/main/scala/com/redhat/et/testcases/hello.scala
@@ -0,0 +1,7 @@
+package com.redhat.et.testcases
+
+class Hello extends Testcase {
+  def run {
+    Console.println("Hello, world!")
+  }
+}
